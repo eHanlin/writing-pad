066bb842ad169aad0d9535362728e3bebb7c07d1
Sparrow.jang <sparrow.jang@gmail.com>
2018-01-18 11:32:45 +0800

    Add a field of id

--- src/js/components/BaseDrawingBoardControl.js
+++ src/js/components/BaseDrawingBoardControl.js
@@ -3,7 +3,8 @@
 let BaseDrawingBoardControl = DrawingBoard.Control.extend({
 
   initializeDefault:function() {
-    if (this.opts && this.opts) this.$el.attr('data-tip-text', this.opts.tipText || this.DEFAULT_TIP_TEXT ||'');
+    if (this.opts) this.$el.attr('data-tip-text', this.opts.tipText || this.DEFAULT_TIP_TEXT ||'');
+    if (this.opts && this.opts.id) this.id = this.opts.id;
   }
 
 });
